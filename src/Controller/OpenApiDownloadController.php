9e9b6b07e9b9c71da154cb809796ae3ec8d665ca
Ted Bowman <ted.git@tedbow.com>
2017-05-20 14:30:31 -0400

    Add link to JSON API direct dowload

--- src/Controller/OpenApiDownloadController.php
+++ src/Controller/OpenApiDownloadController.php
@@ -13,15 +13,28 @@ class OpenApiDownloadController extends ControllerBase {
   public function downloadsList() {
     $return['direct_download'] = [
       '#type' => 'markup',
-      '#markup' => '<h2>' . $this->t('Open API files') . '</h2>' .
+      '#markup' => '<h2>' . $this->t('OpenAPI files') . '</h2>' .
         // @todo Which page should the docs link to?
-        '<p>' . $this->t('The following links provide the REST API resources documented in <a href=":open_api_spec">Open API(fka Swagger)</a> format.', [':open_api_spec' => 'https://github.com/OAI/OpenAPI-Specification/tree/OpenAPI.next']) . ' ' .
+        '<p>' . $this->t('The following links provide the REST or JSON API resources documented in <a href=":open_api_spec">OpenAPI(fka Swagger)</a> format.', [':open_api_spec' => 'https://github.com/OAI/OpenAPI-Specification/tree/OpenAPI.next']) . ' ' .
         $this->t('This JSON file can be used in tools such as the <a href=":swagger_editor">Swagger Editor</a> to provide a more detailed version of the API documentation.', [':swagger_editor' => 'http://editor.swagger.io/#/']) . '</p>',
     ];
-    $open_api_links['entities'] = [
-      'url' => Url::fromRoute('openapi.rest.entities', [], ['query' => ['_format' => 'json']]),
-      'title' => $this->t('Open API: REST Entities'),
-    ];
+
+    if ($this->moduleHandler()->moduleExists('rest')) {
+      $open_api_links['entities'] = [
+        'url' => Url::fromRoute('openapi.rest.entities', [], ['query' => ['_format' => 'json']]),
+        'title' => $this->t('Open API: REST Entities'),
+      ];
+    }
+
+
+    if ($this->moduleHandler()->moduleExists('jsonapi')) {
+      $open_api_links['jsonapi'] = [
+        'url' => Url::fromRoute('openapi.jsonapi', [], ['query' => ['_format' => 'json']]),
+        'title' => $this->t('Open API: JSON API'),
+      ];
+    }
+
+
     /*$open_api_links['other'] = [
       'url' => Url::fromRoute('openapi.non_entities', [], ['query' => ['_format' => 'json']]),
       'title' => $this->t('Open API: Other resources'),
